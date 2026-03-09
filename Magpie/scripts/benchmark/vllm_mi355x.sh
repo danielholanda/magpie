0dc71bde3fd0a489b8310f36d7bf856b6af52698
Hao Li <18546749+haofrank@users.noreply.github.com>
2026-03-09 22:16:39 +0000

    make vllm scripts more versatile on AMD

--- Magpie/scripts/benchmark/vllm_mi355x.sh
+++ Magpie/scripts/benchmark/vllm_mi355x.sh
@@ -38,8 +38,6 @@ fi
 
 # vLLM optimizations for MI355X
 export VLLM_ROCM_USE_AITER=1
-export VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION=1
-export VLLM_ROCM_USE_AITER_MHA=0
 
 SERVER_LOG=${SERVER_LOG:-/workspace/server.log}
 PORT=${PORT:-8888}
